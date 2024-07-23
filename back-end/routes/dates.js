87028fe910eae6b1e3dc33f244a358c85f9cda52
Markus Evanger <123594081+markusevanger@users.noreply.github.com>
2024-07-23 14:05:24 +0000

    asd

--- back-end/routes/dates.js
+++ back-end/routes/dates.js
@@ -8,7 +8,7 @@ const router = express.Router()
 const isValidDateFormat = (dateString) => {
     const regex = /^\d{4}-\d{2}-\d{2}$/;
     return regex.test(dateString);
-  };
+};
 
 
 // Get single date (formatted like: /2024-07-23) <-- ISO 8601
@@ -16,54 +16,53 @@ router.get("/:date", async (req, res) => {
 
     const requestedDate = req.params.date
 
-    if (!isValidDateFormat(requestedDate)){
+    if (!isValidDateFormat(requestedDate)) {
         res.status(404).send(`Not a valid date ${requestedDate}. Must be of format YYYY-MM-DD`)
     }
 
     let collection = await db.collection("dates")
     let paperCollection = await db.collection("papers")
-    let query = {dateFormatted: req.params.date}
+    let query = { dateFormatted: req.params.date }
     let result = await collection.findOne(query)
- 
-    if (result){
+
+    if (result) {
         res.send(result).status(200)
     }
     const date = new Date(requestedDate)
     const dayIndex = date.getDay()
-    const papers =  paperCollection.find({ releaseDates: dayIndex })
+    const papers = paperCollection.find({ releaseDates: dayIndex })
 
-    const papersData = papers.map((paper) => ({
-        [paper.name] : { 
-            paperName: paper.name,
-            productionStatus: "notStarted", // notStarted, inProduction, done
-            
+    const papersData = papers.map((paper) => {
+        return { 
+          paperName: paper.name,
+          productionStatus: "notStarted", // notStarted, inProduction, done
+          date: date
+        };
+      });
+
+
+
+// Date has never been accessed, create new. 
+if (!result) {
+    try {
+        let dateSchema = {
+            dateFormatted: requestedDate,
+
+            date: date.getDate(),
+            month: date.getMonth(),
+            year: date.getFullYear(),
+            dayIndex: dayIndex,
+            papers: papersData
+
+
         }
-    }))
-
-
-    
-    // Date has never been accessed, create new. 
-    if (!result) {
-        try {
-            let dateSchema = {
-                dateFormatted: requestedDate,
-                
-                date: date.getDate(), 
-                month: date.getMonth(),
-                year: date.getFullYear(),
-                dayIndex: dayIndex,
-                
-                
-            }
-            console.log(`Trying to add paper to ${requestedDate}. \n Data: \n  ${JSON.stringify(dateSchema)}`)
-            result = await collection.insertOne(dateSchema)
-            res.send(result).send(200)
-        } catch (err){
-            console.error(err)
-            res.status(500).send("Error adding paper")
-        }
-
+        console.log(`Trying to add paper to ${requestedDate}. \n Data: \n  ${JSON.stringify(dateSchema)}`)
+        result = await collection.insertOne(dateSchema)
+    } catch (err) {
+        console.error(err)
+        res.status(500).send("Error adding paper")
     }
+}
 })
 
 export default router
