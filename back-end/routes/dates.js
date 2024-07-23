4cf26723ac57a329b1a73d9448380eded1e92158
Markus Evanger <123594081+markusevanger@users.noreply.github.com>
2024-07-23 14:42:10 +0000

    asd

--- back-end/routes/dates.js
+++ back-end/routes/dates.js
@@ -1,75 +1,64 @@
+import express from 'express';
+import db from '../db/connection.js';
 
-import express from "express";
-import db from "../db/connection.js";
-
-const router = express.Router()
+const router = express.Router();
 
 // Utility function to validate date format (YYYY-MM-DD)
-const isValidDateFormat = (dateString) => {
-    const regex = /^\d{4}-\d{2}-\d{2}$/;
-    return regex.test(dateString);
-};
+const isValidDateFormat = (dateString) => /^\d{4}-\d{2}-\d{2}$/.test(dateString);
 
+// Helper function to get transformed day index (Monday is 0, Sunday is 6)
+const getTransformedDayIndex = (date) => (date.getDay() + 6) % 7;
 
 // Get single date (formatted like: /2024-07-23) <-- ISO 8601
-router.get("/:date", async (req, res) => {
-
-    const requestedDate = req.params.date
+router.get('/:date', async (req, res) => {
+    const requestedDate = req.params.date;
 
     if (!isValidDateFormat(requestedDate)) {
-        res.status(404).send(`Not a valid date ${requestedDate}. Must be of format YYYY-MM-DD`)
+        return res.status(400).send(`Not a valid date ${requestedDate}. Must be of format YYYY-MM-DD`);
     }
 
-    let collection = await db.collection("dates")
-    let paperCollection = await db.collection("papers")
-    let query = { dateFormatted: req.params.date }
-    let result = await collection.findOne(query)
-
-    if (result) {
-        res.send(result).status(200)
-    }
-    const date = new Date(requestedDate)
-    const dayIndex = (date.getDay() + 6) % 7
-
-    console.log(`Trying to find papers releasing on day: ${dayIndex}`)
-    const papers = await paperCollection.find({ releaseDates: dayIndex }).toArray()
-
-    const papersData = papers.map((paper) => ({
-        [paper.name] : {
-            nameLowerCase: paper.nameLowerCase,
-            productionStatus: "notStarted"
-        }
-    }))
-
-// Date has never been accessed, create new. 
-if (!result) {
     try {
-        let dateSchema = {
+        const date = new Date(requestedDate);
+        const dayIndex = getTransformedDayIndex(date);
+        const collection = db.collection('dates');
+        const paperCollection = db.collection('papers');
+
+        let result = await collection.findOne({ dateFormatted: requestedDate });
+
+        if (result) {
+            return res.status(200).json(result);
+        }
+
+        console.log(`Trying to find papers releasing on day: ${dayIndex}`);
+        const papers = await paperCollection.find({ releaseDates: dayIndex }).toArray();
+        const papersData = papers.map(paper => ({
+            [paper.name]: {
+                nameLowerCase: paper.nameLowerCase,
+                productionStatus: 'notStarted',
+            },
+        }));
+
+        const dateSchema = {
             dateFormatted: requestedDate,
-
             date: date.getDate(),
-            month: date.getMonth()+1,
+            month: date.getMonth() + 1,
             year: date.getFullYear(),
-            dayIndex: dayIndex,
-            papers: papersData
+            dayIndex,
+            papers: papersData,
+        };
 
+        const insertResult = await collection.insertOne(dateSchema);
 
-        }
-        result = await collection.insertOne(dateSchema)
-        if (!result.ok){
-            res.status(500).send(result)
+        if (insertResult.acknowledged) {
+            result = await collection.findOne({ dateFormatted: requestedDate });
+            return res.status(201).json(result);
         } else {
-            let query = { dateFormatted: req.params.date }
-            let newResult = await collection.findOne(query)
-            res.send(newResult).status(200)
+            return res.status(500).send('Failed to insert date schema');
         }
-        
-
     } catch (err) {
-        console.error(err)
-        res.status(500).send("Error adding paper")
+        console.error(err);
+        return res.status(500).send('Error retrieving or adding date');
     }
-}
-})
+});
 
-export default router
+export default router;
